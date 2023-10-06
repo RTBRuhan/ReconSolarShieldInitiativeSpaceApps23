2910c5bca1d6061a7f9f609750ce59b327b2655d
nayem <nayem@SNIFFER-JR>
2023-10-06 13:00:25 +0600

    Introduce regression algo

---
+++ RSSI webAPI/RSSI webAPI/Controllers/BtRegressionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static RSSI_webAPI.RssiRegressionEngine;
+
+namespace RSSI_webAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+
+public class BtRegressionController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public ActionResult<ModelOutput> Predict([FromBody] ModelInput input)
+    {
+        try
+        {
+            // Call the Predict method to make predictions
+            ModelOutput prediction = RssiRegressionEngine.Predict(input);
+            return Ok(prediction);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest($"Prediction error: {ex.Message}");
+        }
+    }
+}
+

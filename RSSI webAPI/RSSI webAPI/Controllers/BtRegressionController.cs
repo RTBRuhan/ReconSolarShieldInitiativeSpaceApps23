9cc39e5f47a1c7bdc5303faa2a2d8bcaebfad38a
nayem <nayem@SNIFFER-JR>
2023-10-06 17:53:52 +0600

    Handle it async

--- RSSI webAPI/RSSI webAPI/Controllers/BtRegressionController.cs
+++ RSSI webAPI/RSSI webAPI/Controllers/BtRegressionController.cs
@@ -11,17 +11,19 @@ public class BtRegressionController : ControllerBase
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public ActionResult<ModelOutput> Predict([FromBody] ModelInput input)
+    public async Task<ActionResult<ModelOutput>> Predict([FromBody] ModelInput input)
     {
         try
         {
             // Call the Predict method to make predictions
-            ModelOutput prediction = RssiRegressionEngine.Predict(input);
+            ModelOutput prediction = await Task.Run(
+                () => RssiRegressionEngine.Predict(input)
+            );
             return Ok(prediction);
         }
         catch (Exception ex)
         {
-            return BadRequest($"Prediction error: {ex.Message}");
+            return BadRequest(ex.Message);
         }
     }
 }
